88f06417ecd0232e190efb7413fc872c07b683f6
HarryCusworth <harry.cusworth@protonmail.com>
2021-02-09 14:05:28 +0000

    trying to make testing work with PDO

--- test/collection_main.php
+++ test/collection_main.php
@@ -4,14 +4,21 @@ require '../collection_main.php';
 
 use PHPUnit\Framework\TestCase;
 
+
+$db = new PDO('mysql:host=db; dbname=arkham_lcg_scenarios', 'root', 'password');
 class Collection_main extends TestCase
 {
 
 
     public function test_getting_campaign_names_malformed()
     {
+        $db = new PDO('mysql:host=db; dbname=arkham_lcg_scenarios', 'root', 'password');
+        $campaigns = $db->prepare("SELECT DISTINCT `cycle` FROM `scenarios`;");
+        $campaigns->setFetchMode(PDO::FETCH_ASSOC);
+        $campaigns->execute();
+        $results = $campaigns->fetchAll();
         $input = 16.1;
-        $this->expectException(TypeError::PDOException);
+        $this->expectException(TypeError::ArgumentCountError);
 
         $result = getCampaignNames($input);
     }
@@ -19,4 +26,3 @@ class Collection_main extends TestCase
 
 
 
-php

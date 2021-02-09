21dae553e5a42ac2ed2aab8ff070a324eb1ff776
HarryCusworth <harry.cusworth@protonmail.com>
2021-02-09 13:36:45 +0000

    finalised mobile and fonts, tidied css selctors

--- test/collection_main.php
+++ test/collection_main.php
@@ -6,4 +6,17 @@ use PHPUnit\Framework\TestCase;
 
 class Collection_main extends TestCase
 {
+
+
+    public function test_getting_campaign_names_malformed()
+    {
+        $input = 16.1;
+        $this->expectException(TypeError::PDOException);
+
+        $result = getCampaignNames($input);
+    }
 }
+
+
+
+php

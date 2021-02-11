3b272cfdfebee66f1c54b27b68029a12d00d18be
HarryCusworth <harry.cusworth@protonmail.com>
2021-02-11 10:42:22 +0000

    post rayna check ups. fixed test file names, deleted surpluss files, added numbers to scenarios

--- test/collection_functions.php
+++ test/collection_functions.php
@@ -4,17 +4,17 @@ require '../collection_functions.php';
 
 use PHPUnit\Framework\TestCase;
 
-class Collection_main extends TestCase
+class Collection_functions extends TestCase
 {
 
-    public function test_feedback_malformed()
+    public function test_print_results_malformed()
     {
         $inputOne = 16.1;
         $inputTwo = "twisted";
         $this->expectException(TypeError::class);
         printResults($inputOne,$inputTwo);
     }
-    public function test_success_get_feedback()
+    public function test_print_results_success()
     {
         $smallArray = ["first Camp", "second Camp"];
         $hugeFakeArray= [['name' => 'first fake', 'cycle'=> 'first Camp', 'completed'=> NULL, 'owned'=>NULL],['name' => 'other fake', 'cycle'=> 'second Camp', 'completed'=> True, 'owned'=>NULL]];
@@ -22,14 +22,6 @@ class Collection_main extends TestCase
         $expectedOutput="<div class=\"flexContainer\"><div class='box'><h2>first Camp</h2><div class='scenarioContainer'><h3>  first fake</h3><div class=\"scenarioContentNull\">Not Owned</div><div class=\"scenarioContentNull\">Not Played</div></div></div><div class='box'><h2>second Camp</h2><div class='scenarioContainer'><h3>  other fake</h3><div class=\"scenarioContentNull\">Not Owned</div><div class=\"scenarioContent\">Played</div></div></div></div>";
             $this->assertEquals($expectedOutput,$actualOutput);
     }
-
-
-
-
-
-
-
-
 
 
 }

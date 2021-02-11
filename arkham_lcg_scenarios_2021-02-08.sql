79b9172fda1073f1814b6910cb3d11d5f021f605
HarryCusworth <77239231+HarryCusworth@users.noreply.github.com>
2021-02-11 12:55:42 +0000

    Update arkham_lcg_scenarios_2021-02-08.sql

--- arkham_lcg_scenarios_2021-02-08.sql
+++ arkham_lcg_scenarios_2021-02-08.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.33)
 # Database: arkham_lcg_scenarios
-# Generation Time: 2021-02-08 13:12:50 +0000
+# Generation Time: 2021-02-11 12:53:57 +0000
 # ************************************************************
 
 
@@ -42,29 +42,27 @@ LOCK TABLES `scenarios` WRITE;
 
 INSERT INTO `scenarios` (`id`, `name`, `cycle`, `position`, `standalone`, `owned`, `completed`, `product`)
 VALUES
-	(1,'The Gathering','Night of the Zealot',1,NULL,1,1,'Base Game'),
+	(1,'The Gathering','Night of the Zealot',1,NULL,0,1,'Base Game'),
 	(2,'The Midnight Masks','Night of the Zealot',2,NULL,1,1,'Base Game'),
 	(3,'The Devourer Below','Night of the Zealot',3,NULL,1,1,'Base Game'),
-	(5,'The Miskatonic Museum','The Dunwich Legacy',3,NULL,1,1,'Scenario Pack'),
+	(5,'The Miskatonic Museum','The Dunwich Legacy',3,NULL,0,1,'Scenario Pack'),
 	(6,'The Essex County Express','The Dunwich Legacy',4,NULL,1,1,'Scenario Pack'),
-	(7,'Extracurricular Activity','The Dunwich Legacy',1,NULL,1,1,'Duluxe Expansion'),
-	(8,'The House Always Wins','The Dunwich Legacy',2,NULL,1,1,'Duluxe Expansion'),
+	(7,'Extracurricular Activity','The Dunwich Legacy',1,NULL,1,1,'Deluxe Expansion'),
+	(8,'The House Always Wins','The Dunwich Legacy',2,NULL,1,1,'Deluxe Expansion'),
 	(9,'Blood on the Altar','The Dunwich Legacy',5,NULL,1,1,'Scenario Pack'),
 	(10,'Undimensioned and Unseen','The Dunwich Legacy',6,NULL,1,1,'Scenario Pack'),
-	(11,'Where Doom Awaits','The Dunwich Legacy',7,NULL,1,1,'Scenario Pack'),
 	(12,'Lost in Time and Space','The Dunwich Legacy',8,NULL,1,1,'Scenario Pack'),
-	(13,'Curse of the Rougarou','Sidestories',NULL,1,1,1,'Scenario Pack'),
-	(14,'Carnevale of Horros','Sidestories',NULL,1,1,NULL,'Scenario Pack'),
-	(15,'The Labyrinths of Lunacy','Sidestories',NULL,1,NULL,NULL,'Scenario Pack'),
-	(16,'Guardians of the Abyss','Sidestories',NULL,1,NULL,NULL,'Scenario Pack'),
-	(17,'Curtain Call','The Path to Carcosa',1,NULL,1,NULL,'Duluxe Expansion'),
-	(18,'The Last King','The Path to Carcosa',2,NULL,1,NULL,'Duluxe Expansion'),
+	(17,'Curtain Call','The Path to Carcosa',1,NULL,0,NULL,'Deluxe Expansion'),
+	(18,'The Last King','The Path to Carcosa',2,NULL,1,NULL,'Deluxe Expansion'),
 	(19,'Echoes of the Past','The Path to Carcosa',3,NULL,1,NULL,'Scenario Pack'),
 	(20,'The Unspeakable Oath','The Path to Carcosa',4,NULL,1,NULL,'Scenario Pack'),
 	(21,'A Phantom of Truth','The Path to Carcosa',5,NULL,1,NULL,'Scenario Pack'),
 	(22,'The Pallid Mask','The Path to Carcosa',6,NULL,1,NULL,'Scenario Pack'),
-	(23,'Black Starts Rise','The Path to Carcosa',7,NULL,1,NULL,'Scenario Pack'),
-	(24,'Dim Carcosa','The Path to Carcosa',8,NULL,1,NULL,'Scenario Pack');
+	(23,'Black Stars Rise','The Path to Carcosa',7,NULL,1,NULL,'Scenario Pack'),
+	(24,'Dim Carcosa','The Path to Carcosa',8,NULL,1,NULL,'Scenario Pack'),
+	(25,'Curse of the Rougarou','Sidestories',NULL,1,1,1,'Scenario Pack'),
+	(26,'Carnevale of Horros','Sidestories',NULL,1,1,NULL,'Scenario Pack'),
+	(27,'The Labyrinths of Lunacy','Sidestories',NULL,1,NULL,NULL,'Scenario Pack');
 
 /*!40000 ALTER TABLE `scenarios` ENABLE KEYS */;
 UNLOCK TABLES;
